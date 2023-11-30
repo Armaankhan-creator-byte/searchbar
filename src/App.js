db3d2799ba33502390b93e024e1a0692a6e280c3
armaan khan <armaankhan991048@gmail.com>
2023-11-30 11:41:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
 
 function App() {
+let g=(()=>{
+  const ele=document.getElementById("ap");
+  const cir=document.getElementById("cir");
+  if(ele){
+    ele.style.display="none";
+    cir.style.display="block";
+    
+  }
+})
+let h=(()=>{
+  const cir=document.getElementById("cir")
+  const ele=document.getElementById("ap");;
+  if(cir){
+    ele.style.display="flex";
+    cir.style.display="none";
+   
+
+  }
+})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     
+    <>
+      <div className="wrapper" >
+        <div className="circle" id="cir" onClick={h}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
+        </div>
+        <div className="input"   id="ap">
+          <input type="text"   placeholder="Type here..." ></input>
+          <div className="search"  onClick={g}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
+      </div>
+        </div>
+     </div>  
+    </>
+    
   );
 }
 
